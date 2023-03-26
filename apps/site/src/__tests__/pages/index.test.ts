ba1380a71480ab9c7f973d260fcc601bfb5eb67d
MH4GF <h.miyagi.cnw@gmail.com>
2023-03-26 13:42:33 +0900

    feat: ダークテーマ切り替え時のテストを追加

--- apps/site/src/__tests__/pages/index.test.ts
+++ apps/site/src/__tests__/pages/index.test.ts
@@ -1,7 +1,31 @@
+import type { Page } from '@playwright/test'
 import { expect, test } from '@playwright/test'
 
-test('トップページを開く', async ({ page }) => {
+const setup = async (page: Page, isMobile: boolean) => {
   await page.goto('/')
+
+  const clickThemeToggleButton = async () => {
+    if (isMobile) {
+      const menuButton = page.getByRole('button', { name: 'Open Menu' })
+      await menuButton.click()
+    }
+    const toggleButton = page.getByRole('button', { name: 'light' })
+    await toggleButton.click()
+  }
+
+  return { clickThemeToggleButton }
+}
+
+test('トップページを開く', async ({ page, isMobile }) => {
+  await setup(page, isMobile)
+
   expect(page.getByRole('heading', { name: 'Hirotaka Miyagi' }))
+  await expect(page).toHaveScreenshot()
+})
+
+test('テーマ切り替えができる', async ({ page, isMobile }) => {
+  const { clickThemeToggleButton } = await setup(page, isMobile)
+  await clickThemeToggleButton()
+
   await expect(page).toHaveScreenshot()
 })
